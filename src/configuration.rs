9647efde4514dc57cc0b7d7921d5c745f63fe688
k0pernicus <antonin.carette@gmail.com>
2017-01-11 23:03:03 +0100

    Change code to manipulate the content of the .gpm configuration file

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,25 +1,84 @@
+use {ConfigurationContent, ConfigurationFile};
 use chrono::DateTime;
 use chrono::offset::utc::UTC;
-use ConfigurationContent;
+use rustc_serialize::Encodable;
+use std::error::Error;
+use std::fmt;
+use std::result;
+use toml::Value;
 
-struct Entry {
-    name: String,
-    path: String,
-    created: DateTime<UTC>,
-    updated: DateTime<UTC>,
+#[derive(RustcEncodable)]
+pub struct Entry {
+    pub name: String,
+    pub path: String,
+    pub created: DateTime<UTC>,
+    pub updated: DateTime<UTC>,
 }
 
-enum ConfigureContentError {
+type Result<T> = result::Result<T, ConfigureContentError>;
+
+#[derive(Debug)]
+pub enum ConfigureContentError {
     BadPosition(String),
+    EncodingError(String),
     KeyAlreadyExists(String),
     UnknownKey(String),
 }
 
-trait ConfigureContent {
-    fn add_watch_entry(&self, key: String, value: &Entry) -> Result<(), ConfigureContentError>;
-    fn add_ignored_entry(&self, key: String, value: &Entry) -> Result<(), ConfigureContentError>;
-    fn remove_watch_entry(&self, key: String) -> Result<(), ConfigureContentError>;
-    fn remove_ignored_entry(&self, key: String) -> Result<(), ConfigureContentError>;
-    fn watch_to_ignored_entry(&self, key: String) -> Result<(), ConfigureContentError>;
-    fn ignored_to_watch_entry(&self, key: String) -> Result<(), ConfigureContentError>;
+impl fmt::Display for ConfigureContentError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            ConfigureContentError::BadPosition(ref e) => e.fmt(f),
+            ConfigureContentError::EncodingError(ref e) => e.fmt(f),
+            ConfigureContentError::KeyAlreadyExists(ref e) => e.fmt(f),
+            ConfigureContentError::UnknownKey(ref e) => e.fmt(f),
+        }
+    }
+}
+
+
+pub trait ConfigureContent {
+    fn add_watch_entry(&mut self, key: &str, value: &Entry) -> Result<()>;
+    fn add_ignored_entry(&self, key: &str, value: &Entry) -> Result<()>;
+    fn remove_watch_entry(&self, key: &str) -> Result<()>;
+    fn remove_ignored_entry(&self, key: &str) -> Result<()>;
+    fn watch_to_ignored_entry(&self, key: &str) -> Result<()>;
+    fn ignored_to_watch_entry(&self, key: &str) -> Result<()>;
+}
+
+impl ConfigureContent for ConfigurationContent {
+    fn add_watch_entry(&mut self, key: &str, value: &Entry) -> Result<()> {
+        if self.contains_key(key) {
+            return Err(ConfigureContentError::KeyAlreadyExists(String::from(key)));
+        }
+        let mut encoder = ConfigurationFile::new();
+        match value.encode(&mut encoder) {
+            Ok(_) => {
+                self.insert(String::from(key), Value::Table(encoder.toml));
+                Ok(())
+            },
+            Err(error) => Err(ConfigureContentError::EncodingError(String::from(error.description()))),
+        }
+    }
+
+    fn add_ignored_entry(&self, key: &str, value: &Entry) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn remove_watch_entry(&self, key: &str) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn remove_ignored_entry(&self, key: &str) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn watch_to_ignored_entry(&self, key: &str) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn ignored_to_watch_entry(&self, key: &str) -> Result<()> {
+        unimplemented!()
+    }
+
 }
