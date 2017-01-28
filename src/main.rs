3e5d43f19caf970d1546c6a822ca56a3d559804f
k0pernicus <antonin.carette@gmail.com>
2017-01-28 23:50:49 +0100

    RustFmt

--- src/main.rs
+++ src/main.rs
@@ -116,8 +116,12 @@ fn main() {
     // Get statuses
     if matches.is_present(commands::STATUS_SUBCMD) {
         println!("[DEBUG] Got {} command !", commands::STATUS_SUBCMD);
-        let get_only_dirty = ! matches.subcommand_matches(commands::STATUS_SUBCMD).unwrap().is_present(commands::STATUS_SUBCMD_CLEAN_FLAG);
-        let get_only_clean = ! matches.subcommand_matches(commands::STATUS_SUBCMD).unwrap().is_present(commands::STATUS_SUBCMD_DIRTY_FLAG);
+        let get_only_dirty = !matches.subcommand_matches(commands::STATUS_SUBCMD)
+            .unwrap()
+            .is_present(commands::STATUS_SUBCMD_CLEAN_FLAG);
+        let get_only_clean = !matches.subcommand_matches(commands::STATUS_SUBCMD)
+            .unwrap()
+            .is_present(commands::STATUS_SUBCMD_DIRTY_FLAG);
         git::get_statuses_from(&vec_path_watched, get_only_clean, get_only_dirty);
     }
 
